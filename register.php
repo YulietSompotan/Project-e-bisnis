603fec1f52a2d4eab1bbbfda400274fe53362cfa
adelclra <adelclangitan@gmail.com>
2025-05-23 02:16:33 +0800

    php pada register

--- register.php
+++ register.php
@@ -1,3 +1,42 @@
+<?php
+// Database connection
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "ebisnis";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+$error = "";
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password'];
+
+    if ($password !== $confirmPassword) {
+        $error = "Password and confirmation do not match!";
+    } else {
+        $check = mysqli_query($conn, "SELECT * FROM user WHERE username='$username'");
+        if (mysqli_num_rows($check) > 0) {
+            $error = "Username is already taken.";
+        } else {
+            $hashedPassword = md5($password); 
+            $query = "INSERT INTO user (username, password) VALUES ('$username', '$hashedPassword')";
+            if (mysqli_query($conn, $query)) {
+                header("Location: login.php?success=1");
+                exit();
+            } else {
+                $error = "An error occurred: " . mysqli_error($conn);
+            }
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +48,11 @@
 </head>
 <body>
   <div class="login-box">
-    <form method="POST" action="register.php">
+    <?php if ($error): ?>
+      <p style="color:red;"><?= $error ?></p>
+    <?php endif; ?>
+
+    <form method="POST" action="">
       <h2>Register</h2>
 
       <div class="input-box">
@@ -26,7 +69,7 @@
 
       <div class="input-box">
         <span class="icon"><i class="fas fa-lock"></i></span>
-        <input type="password" name="password2" required />
+        <input type="password" name="confirm_password" required />
         <label>Confirm Password</label>
       </div>
 
